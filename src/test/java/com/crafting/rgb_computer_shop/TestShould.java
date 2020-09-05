d879e04638cba3d3f8647b72310e0c9d90b64d75
lamak <lamak.qaizar@careem.com>
2020-09-05 21:43:44 +0500

    renamed test

--- src/test/java/com/crafting/rgb_computer_shop/TestShould.java
+++ src/test/java/com/crafting/rgb_computer_shop/TestShould.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class CustomerShould {
+public class TestShould {
 
     @Test
     public void doSomething() {
